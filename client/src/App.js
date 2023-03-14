bd3e39f044faf8fb076c65b2675531bf032dd752
fahad39 <fahadhussain0127@gmail.com>
2023-03-14 19:46:20 +0500

    added front end pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Home from './pages/home';
+import Auth from './pages/auth';
+import CreateRecipe from './pages/createRecipe';
+import SavedRecipe from './pages/savedRecipe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/createRecipe" element={<CreateRecipe />} />
+          <Route path="/savedRecipe" element={<SavedRecipe />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
